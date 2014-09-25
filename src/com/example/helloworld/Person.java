12ec92371ca1a68e5fed1a511aa4e9138930924b
Snahider <snahider@gmail.com>
2014-09-25 17:56:06 -0500

    first Upload

---
+++ src/com/example/helloworld/Person.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
